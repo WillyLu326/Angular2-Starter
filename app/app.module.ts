807043a544b2b0ef0d1038ca80e3466eb65903da
WillyLu326 <luzh0326@gmail.com>
2016-11-23 15:29:24 -0500

    angular2-basic

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,35 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 
-import { UserService } from './shared/services/user.service';
-import { UsersComponent } from './users/users.component';
-import { UserListComponent } from './users/user-list/user-list.component';
-import { UserSingleComponent } from './users/user-single/user-single.component';
-
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/catch';
 
 @NgModule({
   imports: [ 
     BrowserModule,
-    HttpModule,
-    FormsModule,
-    routing
+    FormsModule
   ],
   declarations: [ 
-    AppComponent,
-    UsersComponent,
-    UserListComponent,
-    UserSingleComponent
+    AppComponent
   ],
-  providers: [
-    UserService
-  ],
+  providers: [],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
